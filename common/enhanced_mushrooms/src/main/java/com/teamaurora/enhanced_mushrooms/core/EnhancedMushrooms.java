b068a33e37722edc0aff021ed553165f5b5deb03
Steve <playzzsteven@gmail.com>
2023-10-24 13:38:00 -0400

    Start Novel

---
+++ common/enhanced_mushrooms/src/main/java/com/teamaurora/enhanced_mushrooms/core/EnhancedMushrooms.java
@@ -0,0 +1,5 @@
+package com.teamaurora.enhanced_mushrooms.core;
+
+public class EnhancedMushrooms {
+    public static final String MOD_ID = "enhanced_mushrooms";
+}
---
+++ fabric/enhanced_mushrooms/src/main/java/com/teamaurora/enhanced_mushrooms/core/fabric/EnhancedMushrooms.java
@@ -0,0 +1,10 @@
+package com.teamaurora.enhanced_mushrooms.core.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EnhancedMushrooms implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello World!");
+    }
+}
